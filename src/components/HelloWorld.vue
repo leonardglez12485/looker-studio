b34d551d431fe65fe8633c94e160309f710dad71
Leonard <leonardglez12485@gmail.com>
2024-06-10 16:04:47 -0400

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,11 @@
+
+
 <template>
-  <div class="hello">
+  <h1>Test Powe BI</h1>
+  <div>
+   <section id="container" style="height: 750px"/>
+  </div>
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -27,14 +33,36 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-  </div>
+  </div> -->
 </template>
 
 <script>
+import * as pbi from "powerbi-client";
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  // props: {
+  //   msg: String
+  // }
+  mounted () {
+   const permissions = pbi.models.Permissions.All
+   const config = {
+     type: 'dashboard',
+     tokenType: pbi.models.TokenType.Embed,
+     //accessToken: accessToken,
+     embedUrl: 'https://app.powerbi.com/reportEmbed',
+     id: '5dac7a4a-4452-46b3-99f6-a25915e0fe55',
+     pageView: 'fitToWidth',
+     permissions: permissions,
+  };
+   let powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
+   const dashboardContainer = document.getElementById('container');
+   const dashboard = powerbi.embed(dashboardContainer, config);
+
+   dashboard.off("loaded");
+   dashboard.off("rendered");
+   dashboard.on("error", function () {
+     this.dashboard.off("error");
+   });
   }
 }
 </script>
