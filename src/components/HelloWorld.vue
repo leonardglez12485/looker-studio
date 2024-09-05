8d72e48307c6e9689ced76c4a8ee554a2b46c7c3
Leonard <leonardglez12485@gmail.com>
2024-09-05 19:17:24 -0400

    makin power-Bi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,10 +6,13 @@ export default {
 data(){
   return {
   config: {
-    type: 'PoweBIReport',
-    id: 'b2e382aa-3d54-4eb3-9a0c-d998a39f8f40',
-    embedUrl:'https://app.powerbi.com/reportEmbed?reportId=b2e382aa-3d54-4eb3-9a0c-d998a39f8f40&autoAuth=true&ctid=760fe82f-1c6d-4e38-9fa9-994e8f3fcb10',
-    permissions: pbi.models.Permissions.READ,
+    type: 'dashboard',
+    tokenType: pbi.models.TokenType.Embed,
+    accessToken:  "autoAuth=true&ctid=760fe82f-1c6d-4e38-9fa9-994e8f3fcb10",
+    id: "b2e382aa-3d54-4eb3-9a0c-d998a39f8f40",
+    embedUrl:"https://app.powerbi.com/reportEmbed",
+    pageView: 'fitToWidth',
+    permissions: pbi.models.Permissions.All,
     settings: {
           filterPaneEnabled: true,
           navContentPaneEnabled: true,
@@ -21,20 +24,52 @@ mounted() {
     this.embedDashboard();
   },
   methods: {
-    embedDashboard() {
-      const embedContainer = this.$refs.container;
-      // const embedContainer = document.getElementById('container');
-      const powerbi = new pbi.service.Service(
-        pbi.factories.factories.hpmFactory,
-        pbi.factories.factories.wpmpFactory,
-        pbi.factories.factories.routerFactory
+     embedDashboard() {
+    //  const embedContainer = this.$refs.container;
+     
+      let powerbi = new pbi.service.Service(
+        pbi.factories.hpmFactory,
+        pbi.factories.wpmpFactory,
+        pbi.factories.routerFactory
       );
+      const embedContainer = document.getElementById('container');
       const dashboard = powerbi.embed(embedContainer, this.config);
       dashboard.off("loaded");
       dashboard.off("rendered");
       dashboard.on("error", function () {
       this.dashboard.off("error");
+  });
+
+  const dashboardRef = powerbi.embed(embedContainer, this.config);
+   dashboardRef.off("loaded");
+   dashboardRef.off("rendered");
+   dashboardRef.on("error", function () {
+   this.dashboardRef.off("error");
    });
+
+//    let powerbi = new pbi.service.Service(pbi.factories.hpmFactory, pbi.factories.wpmpFactory, pbi.factories.routerFactory);
+//    const dashboardContainer = document.getElementById('container');
+//   try {
+//     const dashboard = powerbi.embed(dashboardContainer, this.config);
+//    dashboard.off("loaded");
+//    dashboard.off("rendered");
+//    dashboard.on("error", function () {
+//      this.dashboard.off("error");
+//    });
+//   } catch (error) {
+//     console.log(error)
+//   }
+
+// try {
+//   const dashboardRef = powerbi.embed(dashboardContainer, this.config);
+//    dashboardRef.off("loaded");
+//    dashboardRef.off("rendered");
+//    dashboardRef.on("error", function () {
+//    this.dashboardRef.off("error");
+//    });
+// } catch (error) {
+//   console.log(error)
+// }
     },
   },
 }
@@ -59,12 +94,22 @@ a {
 </style>
 
 <template>
-  <h1>Test Power-BI</h1>
-  <div>
-   <!-- <section id="container" style="height: 750px"/> -->
+  <h1>Test Looker Studio</h1>
+  <div class="test-1">
+    <h2>Dashboard I</h2>
+   <!--<section id="container" style="height: 750px"/>-->
    <!-- <section ref="container" style="height: 750px"/> -->
-   <iframe src="https://app.powerbi.com/reportEmbed?reportId=d5b2554b-0d31-4e19-a310-c3610edfaaa3&autoAuth=true&ctid=760fe82f-1c6d-4e38-9fa9-994e8f3fcb10" width="100%" height="600px"></iframe>
+   <iframe width="1200" height="950" src="https://lookerstudio.google.com/embed/reporting/0f3d48b7-a52a-4542-98a0-a0182ca61514/page/BHP6D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
+  </div>
+
+  <div class="test-2">
+    <h2>Dashboard II</h2>
+    <iframe width="1200" height="950" src="https://lookerstudio.google.com/embed/reporting/0bd48870-462f-412f-b85b-bf9d5577e3d9/page/I4T6D" frameborder="0" style="border:0" allowfullscreen sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"></iframe>
   </div>
 </template>
+
+<style scoped>
+@import '../style.css';
+</style>
 
 
